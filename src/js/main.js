b6a6c1697f3ccb7c6c70f91c40ed0075549628b0
Svetlana Terenteva <gubinas83@gmail.com>
2021-01-24 02:56:27 +0300

    feat: add swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-console.log('Hello, world!');
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+              clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '"></span>';
+          },
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.news__banner-slider-next',
+      prevEl: '.news__banner-slider-prev',
+    },
+});
+
+console.log("oops!");
