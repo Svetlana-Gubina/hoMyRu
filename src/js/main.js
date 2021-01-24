1985db262bf05bb39f2cf01ba214f9dffdf9f9c2
Svetlana Terenteva <gubinas83@gmail.com>
2021-01-24 23:43:12 +0300

    feat: add tiny slider

--- src/js/main.js
+++ src/js/main.js
@@ -19,4 +19,101 @@ var mySwiper = new Swiper('.swiper-container', {
     },
 });
 
-console.log("oops!");
+
+// TNS
+
+var slider1 = tns({
+    container: '#my-slider-1',
+    items: 1,
+    nav: false, 
+    slideBy: 'page',
+    speed: 400,
+    mouseDrag: true,
+    controlsContainer: "#customize-controls-1",
+    responsive: {
+      320: {
+        center: true,
+        gutter: 20,
+        items: 1
+      },
+      750: {
+        center: false,
+        items: 3,
+      },
+      980: {
+        items: 4
+      }
+    }
+});
+
+var slider2 = tns({
+    container: '#my-slider-2',
+    items: 1,
+    nav: false, 
+    slideBy: 'page',
+    speed: 400,
+    mouseDrag: true,
+    controlsContainer: "#customize-controls-2",
+    responsive: {
+      320: {
+        center: true,
+        gutter: 20,
+        items: 1
+      },
+      750: {
+        center: false,
+        items: 3,
+      },
+      980: {
+        items: 4
+      }
+    }
+});
+
+var slider3 = tns({
+    container: '#my-slider-3',
+    items: 1,
+    nav: false, 
+    slideBy: 'page',
+    speed: 400,
+    mouseDrag: true,
+    controlsContainer: "#customize-controls-3",
+    responsive: {
+      320: {
+        center: true,
+        gutter: 20,
+        items: 1
+      },
+      750: {
+        center: false,
+        items: 3,
+      },
+      980: {
+        items: 4
+      }
+    }
+});
+
+var slider4 = tns({
+    container: '#my-slider-4',
+    items: 1,
+    nav: false, 
+    slideBy: 'page',
+    speed: 400,
+    mouseDrag: true,
+    controlsContainer: "#customize-controls-4",
+    responsive: {
+      320: {
+        center: true,
+        gutter: 20,
+        items: 1
+      },
+      750: {
+        center: false,
+        items: 3,
+      },
+      980: {
+        items: 4
+      }
+    }
+});
